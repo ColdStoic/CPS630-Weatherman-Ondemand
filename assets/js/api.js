6e0fd1eeb1fdd75ac3c4eab206d6b58185d2f7b3
coldstoic <coldstoic@gmail.com>
2019-02-08 23:22:18 -0500

    changed to citylist.min, retrieves slice by location name, calls api using id

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,11 +1,8 @@
 // Onload.
 document.addEventListener("DOMContentLoaded", function(event) {
     var xmlhttp = new XMLHttpRequest();
-    var jsonURL = "https://jsonplaceholder.typicode.com/todos/1";
+    var jsonURL = "";
     var jsonResponse;
-
-    var jsonCityURL = "assets/json/cityList.json";
-    //var jsonCityResponse = JSON.parse(cityList);
     
     xmlhttp.onreadystatechange = function() {
         if (xmlhttp.readyState== 4 && xmlhttp.status== 200) {
@@ -14,19 +11,37 @@ document.addEventListener("DOMContentLoaded", function(event) {
         }
     }
 
+    var ID = getSliceByLocation("Toronto").id;
+    jsonURL = getApiUrl(ID);
+    console.log(jsonURL);
+
     xmlhttp.open("GET", jsonURL, true);
     xmlhttp.send();
-
-    //printConsole(cityList);
 });
 
 // Print text to console.
 function printConsole(text) {
-    console.log(text)
+    console.log(text);
 }
 
 // Search bar handler.
 function searchHandler() {
     var x = document.getElementById("search-input").value;
     document.getElementById("text-output").innerHTML = x;
+}
+
+function getSliceByLocation(country) {
+    return cityList.filter(
+        function(cityList) {
+            return cityList.name == country
+        }
+    )[0];
+}
+
+function toUpperFirst(str) {
+    return str.toLowerCase().split(' ').map((s) => s.charAt(0).toUpperCase() + s.substring(1)).join(' ');
+}
+
+function getApiUrl(id) {
+    return "https://api.openweathermap.org/data/2.5/weather?id=" + id + "&APPID=4c06bfe661f0b300a0f60bc62534ad7d";
 }
